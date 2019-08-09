483a0252b3de2b6b7fec3de0d410fe9f51704d3e
Woshiajuana <zhigang.chen@owulia.com>
2019-08-09 17:30:03 +0800

    submit package and build

---
+++ build/index.js
@@ -0,0 +1,2 @@
+
+require('./webpack.config');
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -3,8 +3,26 @@ const webpack = require('webpack');
 const path = require('path');
 const fs = require('fs-extra');
 const output = require('wow-cmd').output;
+const cmdPath = process.cwd();
 
+const
 
+(function (dir) {
+    dir = dir || '.';
+    const directory = path.join(cmdPath, 'src/views', dir);
+    console.log(directory);
+    fs.readdirSync(directory).forEach((file) => {
+        const fullPath = path.join(directory, file);
+        const fileStat = fs.statSync(fullPath);
+        const fileExtName = path.extname(fullPath);
+        if (fileStat.isFile() && fileExtName === '.vue') {
+
+        } else if (['components'].indexOf() === -1) {
+
+        }
+    });
+
+})();
 
 const webpackConfig = {
 
