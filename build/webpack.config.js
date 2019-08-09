68aab33358b9829ca10ba7fa37d7f50ab2408398
Woshiajuana <zhigang.chen@owulia.com>
2019-08-09 16:52:19 +0800

    submit

---
+++ build/webpack.config.js
@@ -0,0 +1,11 @@
+
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs-extra');
+const output = require('wow-cmd').output;
+
+const webpackConfig = {
+
+};
+
+module.exports = webpackConfig;
