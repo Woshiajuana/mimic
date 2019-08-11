b77415a935cee36b610f96659b18dcefebc8d98e
Woshiajuana <zhigang.chen@owulia.com>
2019-08-11 13:42:48 +0800

    sub mit and build and config

--- build/config.js
+++ build/config.js
@@ -1,7 +1,7 @@
 
 // 整个打包项目的配置文件 该配置只在打包的时候参与作用 不会打包进入实际业务代码
 
-// webpack 配置
+// directory 配置
 const directoryConfig = {
     // 根目录遍历
     rootDirectoryPath: '/src/views',
@@ -13,6 +13,17 @@ const directoryConfig = {
     rootOutputPath: '/dist',
 };
 
+// App 配置
+const applicationConfig = {
+    // APPID
+    mk: {
+        // app名称
+        name: '码可',
+        
+    }
+};
+
 module.exports = {
     directoryConfig,
+    applicationConfig,
 };
