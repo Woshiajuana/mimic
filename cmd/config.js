ed47148020f2a68582b0f6a340e4820c537f1437
Woshiajuana <zhigang.chen@owulia.com>
2019-08-11 14:51:22 +0800

    submit config and cmd

--- cmd/config.js
+++ cmd/config.js
@@ -1,6 +1,20 @@
 
 // 整个打包项目的配置文件 该配置只在打包的时候参与作用 不会打包进入实际业务代码
 
+// 获取 ip
+const ip = (() => {
+    let interfaces = require('os').networkInterfaces();
+    for(let devName in interfaces){
+        let iface = interfaces[devName];
+        for(let i=0; i < iface.length; i++){
+            let alias = iface[i];
+            if(alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal){
+                return alias.address;
+            }
+        }
+    }
+})();
+
 // directory 配置
 const directoryConfig = {
     // 根目录遍历
