c8e5173875f66155fc68eab54b8f45f888179d64
Woshiajuana <zhigang.chen@owulia.com>
2019-11-04 18:44:22 +0800

    subcode

--- cmd/oss.cmd.js
+++ cmd/oss.cmd.js
@@ -1,12 +1,9 @@
 
-const crypto = require('crypto');
 const fs = require('fs-extra');
 const path = require('path');
 const output = require('wow-cmd').output;
-const { directoryConfig, applicationConfig } = require('./config');
 const cmdPath = process.cwd();
-const { rootOutputPath } = directoryConfig;
-const oss = require('ali-oss');
+const OSS = require('ali-oss');
 
 const Handle = (options, data, next) => {
     try {
@@ -30,13 +27,43 @@ const Handle = (options, data, next) => {
             });
             return objParams;
         })(params);
-
+        output.success('region=>', region);
+        output.success('accessKeyId=>', accessKeyId);
+        output.success('accessKeySecret=>', accessKeySecret);
+        output.success('bucket=>', bucket);
+        let client = new OSS({
+            region,
+            accessKeyId,
+            accessKeySecret,
+            bucket,
+        });
+        // (async () => {
+            client.put('test.txt', 'xxx');
+            // await client.put('test.txt', 'xxx');
+            // output.success('oss.cmd=>', `上次成功`);
+        // })();
     } catch (e) {
-        output.error('oss.cmd=>', `md5计算错误：${e}`);
+        output.error('oss.cmd=>', `上传失败：${e}`);
     } finally {
         next();
     }
 };
+
+// let client = new OSS({
+//     region: '<Your region>',
+//     accessKeyId: '<Your AccessKeyId>',
+//     accessKeySecret: '<Your AccessKeySecret>',
+//     bucket: '<Your bucket name>',
+// });
+//
+// async function put () {
+//     try {
+//         let result = await client.put('object-name', 'local-file');
+//         console.log(result);
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
 
 // 参数 options
 Handle.options = {
